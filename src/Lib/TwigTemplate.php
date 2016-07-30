0740a83279913c92e56d37532bf81a83f53234b2
Jamie Isaacs <pdt256@gmail.com>
2016-07-29 17:17:25 -0700

    Adding TwigTemplate helper.

---
+++ src/Lib/TwigTemplate.php
@@ -0,0 +1,44 @@
+<?php
+namespace inklabs\KommerceTemplates\Lib;
+
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+use Twig_SimpleFilter;
+
+class TwigTemplate
+{
+    /** @var Twig_Environment */
+    private $twigEnvironment;
+
+    public function __construct($paths = [])
+    {
+        $twigLoader = new Twig_Loader_Filesystem($paths);
+        $this->twigEnvironment = new Twig_Environment($twigLoader);
+        $this->addCustomFilters();
+    }
+
+    public function enableDebug()
+    {
+        $this->twigEnvironment->enableDebug();
+    }
+
+    /**
+     * @return Twig_Environment
+     */
+    public function getTwigEnvironment()
+    {
+        return $this->twigEnvironment;
+    }
+
+    private function addCustomFilters()
+    {
+        $displayPriceFilter = new Twig_SimpleFilter(
+            'displayPrice',
+            function ($price) {
+                return '$' . number_format(($price / 100), 2);
+            }
+        );
+
+        $this->twigEnvironment->addFilter($displayPriceFilter);
+    }
+}
