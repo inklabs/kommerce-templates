6739774f67ffe46474e2f311ad8f98ebfef0a091
Jamie Isaacs <pdt256@gmail.com>
2016-07-31 15:02:02 -0700

    Adding more pages. Cart pages. New layouts. New helpers. New translations package.

--- src/Lib/TwigTemplate.php
+++ src/Lib/TwigTemplate.php
@@ -2,6 +2,7 @@
 namespace inklabs\KommerceTemplates\Lib;
 
 use Twig_Environment;
+use Twig_Extensions_Extension_I18n;
 use Twig_Loader_Filesystem;
 use Twig_SimpleFilter;
 
@@ -16,7 +17,9 @@ class TwigTemplate
 
         $twigLoader = new Twig_Loader_Filesystem($paths);
         $this->twigEnvironment = new Twig_Environment($twigLoader);
+        $this->twigEnvironment->addGlobal('CSRF_TOKEN', csrf_token());
         $this->addCustomFilters();
+        $this->addExtensions();
     }
 
     public function enableDebug()
@@ -51,5 +54,10 @@ class TwigTemplate
     {
         $baseThemePath = __DIR__ . '/../../themes/base/templates';
         $paths[] = $baseThemePath;
+    }
+
+    private function addExtensions()
+    {
+        $this->twigEnvironment->addExtension(new Twig_Extensions_Extension_I18n());
     }
 }
