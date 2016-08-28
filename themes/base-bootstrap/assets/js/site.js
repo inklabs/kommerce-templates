50f86ac43969eb2fd378110d0398519c48a88051
Jamie Isaacs <pdt256@gmail.com>
2016-08-28 16:40:10 -0700

    Cleaning up spinner javascript class.

--- themes/base-bootstrap/assets/js/site.js
+++ themes/base-bootstrap/assets/js/site.js
@@ -1,33 +1,38 @@
-$(function() {
-    addTooltip();
-    addSpinners();
+$(function () {
+    Kommerce.addTooltip();
+    Kommerce.addSpinners();
 });
 
-function addTooltip() {
-    $("[data-toggle='tooltip']").tooltip();
-}
+var Kommerce = Kommerce || {};
 
-function addSpinners() {
-    $(document.body).on('click', 'a.btn, .pagination li:not(.disabled,.active) a', function (e) {
-        addButtonSpinner($(this));
-    });
+Kommerce = new function () {
+    this.addTooltip = function () {
+        $("[data-toggle='tooltip']").tooltip();
+    };
 
-    $(document.body).on('submit', 'form', function (e) {
-        var $button = $(this).find('.btn[type=submit]');
-        if ($button.length) {
-            addButtonSpinner($button);
+    this.addSpinners = function () {
+        var $this = this;
+        $(document.body).on('click', 'a.btn, .pagination li:not(.disabled,.active) a', function (e) {
+            $this.addButtonSpinner($(this));
+        });
+
+        $(document.body).on('submit', 'form', function (e) {
+            var $button = $(this).find('.btn[type=submit]');
+            if ($button.length) {
+                $this.addButtonSpinner($button);
+            }
+        });
+    };
+
+    this.addButtonSpinner = function ($button, classString) {
+        classString = classString || 'fa fa-spinner fa-pulse';
+        $button.addClass('disabled');
+        var $icon = $button.find('i');
+        if ($icon.length) {
+            $icon.attr('data-class', $icon.attr('class'));
+            $icon.attr('class', classString);
+        } else {
+            $button.prepend('<i class="' + classString + '"></i> ');
         }
-    });
-}
-
-function addButtonSpinner($button, classString) {
-    classString = classString || 'fa fa-spinner fa-pulse';
-    $button.addClass('disabled');
-    $icon = $button.find('i');
-    if ($icon.length) {
-        $icon.attr('data-class', $icon.attr('class'));
-        $icon.attr('class', classString);
-    } else {
-        $button.prepend('<i class="' + classString + '"></i> ');
-    }
-}
+    };
+};
