58182a699cc4ede4258d651af23fdc088eb66de6
Jamie Isaacs <pdt256@gmail.com>
2016-12-25 16:29:42 -0800

    Updating acceptance test for sales tax rules.

--- codeception/acceptance/AdminSettingCest.php
+++ codeception/acceptance/AdminSettingCest.php
@@ -6,6 +6,6 @@ class AdminSettingCest
     {
         $I->wantTo('view sales tax settings');
         $I->amOnPage('/admin/settings/sales-tax');
-        $I->see('Sales Tax Settings');
+        $I->see('Sales Tax Rules');
     }
 }
