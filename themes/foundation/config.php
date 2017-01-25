8e8ec94535017b0dffae1ed3c19102c2351f3c74
Jason Raimondi <jason@raimondi.us>
2017-01-24 22:07:46 -0800

    Wip foundation scss build.

--- themes/foundation/config.php
+++ themes/foundation/config.php
@@ -1,9 +1,15 @@
 <?php
 use inklabs\KommerceTemplates\Lib\TwigThemeConfig;
 
-return new TwigThemeConfig(
+$config = new TwigThemeConfig(
     'Foundation',
     'Deprecated Foundation theme',
     __DIR__,
     TwigThemeConfig::getBaseThemePath()
 );
+
+$config->addScssImportPath(
+    realpath(__DIR__ . '/../../../../../vendor') . '/zurb/foundation/scss'
+);
+
+return $config;
